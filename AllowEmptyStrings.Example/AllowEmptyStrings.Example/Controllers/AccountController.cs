54f3f7fdc41c01b0d3250a6d39df0484b0668688
Tolga <Tolga@LAPTOP-H78R7VMS>
2018-04-12 18:25:30 +0300

    initial

---
+++ AllowEmptyStrings.Example/AllowEmptyStrings.Example/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AllowEmptyStrings.Example.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AllowEmptyStrings.Example.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            return View();
+        }
+    }
+}
---
+++ AllowEmptyStrings.Example/AllowEmptyStrings.Example/Models/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllowEmptyStrings.Example.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email Required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password Required", AllowEmptyStrings = true)]
+        public string Password { get; set; }
+    }
+}
